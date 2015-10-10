3bb0e0b49e766060485f936679ec2e314e869058
lidetao <1209461019@qq.com>
2015-10-11 00:11:49 +0800

    sublime 插件测试
    sublime 插件测试：
    Package Control 插件控制台-ctrl+shift+p--install；
    Emmet html快速编写工具--tab补全代码；
    HTML-CSS-JS Prettify html/css/js格式化插件--Ctrl+Shift+H；

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
-function changeImage()
-{
-element=document.getElementById('myimage')
-if (element.src.match("bulbon"))
-  {
-  element.src="/i/eg_bulboff.gif";
-  }
-else
-  {
-  element.src="/i/eg_bulbon.gif";
-  }
+function changeImage() {
+    element = document.getElementById('myimage')
+    if (element.src.match("bulbon")) {
+        element.src = "/i/eg_bulboff.gif";
+    } else {
+        element.src = "/i/eg_bulbon.gif";
+    }
 }
